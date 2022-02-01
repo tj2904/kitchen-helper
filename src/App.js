c77b274988436e56e41eff75fdf546f8eb745f23
Tim Jackson <tj2904+github@gmail.com>
2022-02-01 22:11:39 +0000

    replace some boilerplate & add temp conversion

--- src/App.js
+++ src/App.js
@@ -1,24 +1,47 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./KH-logo.png";
+import "./App.css";
+import FromCelcius from "./FromCelcius.jsx";
+import RecipeConversion from "./RecipeConversion.jsx";
 
 function App() {
+  const [currentTime, setCurrentTime] = useState(0);
+  const [orginalTemp, setOrginalTemp] = useState('');
+  const [url, setUrl] = useState('');
+
+  useEffect(() => {
+    fetch("/time")
+      .then((res) => res.json())
+      .then((data) => {
+        setCurrentTime(data.time);
+      });
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+  Kitchen Helper
+        <p>The current Unix time is {currentTime} (as supplied by the api).</p>
       </header>
+
+
+      <h4>Oven temperature Conversion:</h4>
+      Enter a temperature to convert from Celcius: <input onChange={event => setOrginalTemp(event.target.value)} />
+      <div>
+        <FromCelcius orginal={orginalTemp} />
+      </div>
+
+    <div>
+    <h4>Web Recipe Measurement Conversion:</h4>
+    <div>
+    Recipe URL that you would like converting: <input placeholder="https://www."/> <button  onSubmit={event => setUrl(event.target.value)}> Convert! </button>
+</div>
+      {/* <RecipeConversion /> */}
     </div>
+
+
+    </div> //end of app
   );
 }
 
