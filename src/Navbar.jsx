e92208a01acedc39a390287240dfb8c0392c6912
Tim Jackson <tj2904+github@gmail.com>
2022-02-03 21:52:24 +0000

    add sticky navbar with logo

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -1,11 +1,11 @@
-import logo from "./logo192.png"
+import logo from "./favicon-32x32.png"
 import "./Navbar.css"
 
 function Navbar() {
     return ( 
-    <nav class="navbar ">
+    <nav class="navbar sticky-top ">
   <a class="navbar-brand" href="/">
-    <img src={logo} width="35" height="35" class="d-inline-block align-top" alt="" className="Nav-logo"></img>
+    <img src={logo} width="32px" height="32px" alt="Logo" ></img>
     Kitchen Helper
   </a>
 </nav>
