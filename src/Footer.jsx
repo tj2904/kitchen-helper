bec8f06e175ad1061ab2c5f57edb02af6f16ff45
tj2904 <tj2904+github@gmail.com>
2022-03-09 20:37:09 +0000

    improved formatting

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,16 +1,12 @@
-import "./Footer.css"
+import "./Footer.css";
 
-
-function Footer () {
-return (
-
- <footer class="footer">
-
-   <span class="text-muted">Data from <a href="https://spoonacular.com/food-api">Spoonacular</a>.</span>
-
-</footer>
-
-
-);
+function Footer() {
+  return (
+    <footer class="footer">
+      <span class="text-muted">
+        Data from <a href="https://spoonacular.com/food-api">Spoonacular</a>.
+      </span>
+    </footer>
+  );
 }
 export default Footer;
